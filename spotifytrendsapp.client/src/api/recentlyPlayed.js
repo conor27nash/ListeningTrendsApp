86707c66d905d05631abbeab88e264bd0ab63b63
Conor Nash <ConorN@forrit.com>
2025-07-08 16:42:09 +0100

    missed comment

--- spotifytrendsapp.client/src/api/recentlyPlayed.js
+++ spotifytrendsapp.client/src/api/recentlyPlayed.js
@@ -20,19 +20,3 @@ export const getRecentlyPlayed = async (limit = 50, before = null, after = null)
   }
   return await response.json();
 };
-
-
-// export const getRecentlyPlayed = async (limit = 50, after = null, before = null) => {
-//   const queryParams = new URLSearchParams({ limit: limit.toString() });
-//   if (after) queryParams.append('after', after.toString());
-//   if (before) queryParams.append('before', before.toString());
-  
-//   const url = `${LoginService_BASE_URL}/api/recentlyplayed/recent-tracks?${queryParams}`;
-//   const response = await fetchWithAuth(url, {
-//     method: 'GET', headers: { 'Content-Type': 'application/json' }
-//   });
-//   if (!response.ok) {
-//     throw new Error(`Error fetching recently played: ${response.statusText}`);
-//   }
-//   return await response.json();
-// };
