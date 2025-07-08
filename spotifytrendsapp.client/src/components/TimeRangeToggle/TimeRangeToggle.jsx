9c11179fe9deca8b4158675c053c9528d0a22dac
Conor Nash <ConorN@forrit.com>
2025-07-08 13:23:39 +0100

    Added active to time range short term on load

--- spotifytrendsapp.client/src/components/TimeRangeToggle/TimeRangeToggle.jsx
+++ spotifytrendsapp.client/src/components/TimeRangeToggle/TimeRangeToggle.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './TimeRangeToggle.css';
 
-const TimeRangeToggle = ({ current, onChange }) => (
+const TimeRangeToggle = ({ current = "short_term", onChange }) => (
   <div className="time-toggle">
-    {['short_term', 'medium_term', 'long_term'].map(term => (
-      <button
-        key={term}
-        className={term === current ? 'active' : ''}
-        onClick={() => onChange(term)}
-      >
-        {term.charAt(0).toUpperCase() + term.slice(1)} Term
-      </button>
-    ))}
+    {['short_term', 'medium_term', 'long_term'].map(term => {
+      const label = term.split('_')[0];
+      return (
+        <button
+          key={term}
+          className={term === current ? 'active' : ''}
+          onClick={() => onChange(term)}
+        >
+          {label.charAt(0).toUpperCase() + label.slice(1)} Term
+        </button>
+      );
+    })}
   </div>
 );
 
